fd92fe57db9fe3138535c938fd3be1707771bdf4
Dejihz <olag0002@hz.nl>
2022-02-17 11:18:05 +0100

    Finished week 1

---
+++ app/Models/Grade.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    use HasFactory;
+
+
+    public function addResult($grade){
+        if($this->best_grade < $grade){
+            $this->best_grade = $grade;
+        }
+        if($this->best_grade >= $this->lowest_passing_grade){
+            $this->passed_at = date('Y/m/d');
+        }
+        $this->save();
+    }
+}
